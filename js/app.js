b15ac80400c18e23b0c3a13b7549e56a397f9776
Levi Li <lli@seek.com.au>
2017-12-13 13:20:04 +1100

    UI consumes data which is dispatched by flux

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Hello from './component/Hello';
+import Main from './component/Main';
 
-ReactDOM.render(<Hello />, document.getElementById('react'));
+ReactDOM.render(<Main />, document.getElementById('react'));
---
+++ js/component/Main.js
@@ -0,0 +1,50 @@
+import React from "react";
+import API from "../API";
+import LinkStore from "../stores/LinkStore";
+
+// create another function to always read the links from the store
+let getAppState = () => {
+  return { links: LinkStore.getAll() };
+};
+
+export default class Main extends React.Component{
+  constructor(props) {
+    // in main, it needs to read the data from the store
+    super(props);
+
+    this.state = getAppState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    API.fetchLinks();
+    // register a listener to the store EventEmitter, using the on method
+    LinkStore.on("change", this.onChange);
+  }
+
+  // remove the listener
+  componentWillUnmount() {
+    LinkStore.removeListener("change", this.onChange);
+  }
+
+  onChange() {
+    console.log("4. In the view");
+    this.setState(getAppState());
+  }
+
+  render() {
+    let content = this.state.links.map(link => {
+      return <li key={link._id}>
+               <a href={link.url}>{link.title}</a>
+            </li>;
+    });
+    return (
+      <div>
+        <h3>Links</h3>
+        <ul>
+          {content}
+        </ul>
+      </div>
+    );
+  }
+}
